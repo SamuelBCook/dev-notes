f70576d3fc9a8e0f406409e81819b482251d2773
Samuel Cook <samuel.cook@sulmara.com>
2025-08-19 14:51:42 +0100

    toggle md editor

--- src/notes.py
+++ src/notes.py
@@ -3,13 +3,44 @@ from datetime import datetime
 from backend import write_note_handler
 from pathlib import Path
 
+
+def markdown_view():
+
+    with ui.row().style('width: 100vw; height: 100vh; gap: 10px'):
+        text_input = ui.textarea(label='Markdown Editor', placeholder='Type something...').style('flex: 1; height: 100%;')
+        markdown_preview = ui.markdown('### Lets write some Markdown! ').style('flex: 1; height: 100%; overflow: auto;')
+
+        text_input.on_value_change(lambda e: markdown_preview.set_content(text_input.value))
+
+    return text_input
+
+
+def text_view():
+    text_input = ui.textarea(label='Markdown Editor', placeholder='Type something...').style('flex: 1; height: 100%;')
+
+    return text_input
+
+
+
 def notes_main(output_dir:Path, db_path:Path):
     #ui.link("New notes page...", "/new-note")  # can do to external websites instead
 
-    file_toggle = ui.toggle([".txt", ".md"], value=".txt")  # TBC: add on change to switch between views (md window or not)
-
     today = datetime.today().strftime("%Y-%m-%d")
     default_title = f"Notes {today}"
+
+    with ui.row().style('gap: 10px; align-items: center;'):
+            file_toggle = ui.toggle([".txt", ".md"], value=".txt")
+            save_button = ui.button(
+                "Save",
+                on_click=lambda: write_note_handler(
+                    title_input=title_input,
+                    text_input=text_input,
+                    output_dir=output_dir,
+                    db_path=db_path,
+                    extension=file_toggle.value,
+                ),
+                icon="create",
+            )
 
     title_input = ui.input(
         label="Title",
@@ -20,17 +51,11 @@ def notes_main(output_dir:Path, db_path:Path):
     with ui.row().style('width: 100vw; height: 100vh; gap: 10px'):
         text_input = ui.textarea(label='Markdown Editor', placeholder='Type something...').style('flex: 1; height: 100%;')
         markdown_preview = ui.markdown('### Lets write some Markdown! ').style('flex: 1; height: 100%; overflow: auto;')
+        markdown_preview.visible = False  # start hidden as long as default is .txt
 
         text_input.on_value_change(lambda e: markdown_preview.set_content(text_input.value))
 
-    ui.button(
-            "Save",
-            on_click=lambda: write_note_handler(
-                title_input=title_input,  
-                text_input=text_input,   
-                output_dir=output_dir,
-                db_path=db_path,
-                extension=file_toggle.value,
-            ),
-            icon="create",
-    )
+    def update_preview(e):
+        markdown_preview.visible = (file_toggle.value == ".md")
+
+    file_toggle.on_value_change(update_preview)
